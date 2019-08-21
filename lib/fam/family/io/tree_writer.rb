f68ffc64ba32de26a5851013c22ce4d421dd69be
Brian Peck <peckb@squareup.com>
2019-08-20 20:12:28 -0700

    Pull out tree reader/reader logic

---
+++ lib/fam/family/io/tree_writer.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'fam/family/serialization/family_serializer'
+
+module Fam::Family::IO
+  class TreeWriter
+    class << self
+      def save_to_file(tree, output_path)
+        write_hash_to_file(serialize_tree(tree), output_path)
+      end
+
+      private
+
+      def serialize_tree(tree)
+        Fam::Family::Serialization::FamilySerializer.serialize(family: tree)
+      end
+
+      def write_hash_to_file(output_hash, output_path)
+        Fam::File::Writer.create(path: output_path).write(json_hash: output_hash)
+      end
+    end
+  end
+end
